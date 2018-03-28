4a960d7aa98e5315feb92be0c9360e0ffae6f89a
StarRaven <alvin.gyt@gmail.com>
2018-03-28 14:12:09 -0400

    movie0.json

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,21 +1,35 @@
 import requests
 import json
-    
-max = 514246
+min = 1
+max = 5
+# min = 1
+# max = 100000
+# min = 100001
+# max = 200000
+# min = 200001
+# max = 300000
+# min = 300001
+# max = 400000
+# min = 400001
+# max = 500000
+# min = 500001
+# max = 514246
 
-with open("movie.json","a") as f:
+with open("movie.json", "a") as f:
     f.write('[')
 
-    for num in range(2,max):
-        url='https://api.themoviedb.org/3/movie/'+str(num)+'?api_key=abc69f3218f083159cb0ebe413e122d2&language=en-US' 
+    for num in range(min, max):
+        if num % 1000 == 0:
+            print(num)
+        url = 'https://api.themoviedb.org/3/movie/' + str(num) +'?api_key=abc69f3218f083159cb0ebe413e122d2&language=en-US'
         response = requests.get(url)
         if response.status_code == 200:
-            json.dump(response.json(),f)
+            json.dump(response.json(), f)
             f.write(',')
 
-    url='https://api.themoviedb.org/3/movie/'+str(max)+'?api_key=abc69f3218f083159cb0ebe413e122d2&language=en-US' 
-        response = requests.get(url)
-        if response.status_code == 200:
-            json.dump(response.json(),f)
-            
+    url = 'https://api.themoviedb.org/3/movie/' + str(max) +'?api_key=abc69f3218f083159cb0ebe413e122d2&language=en-US'
+    response = requests.get(url)
+    if response.status_code == 200:
+        json.dump(response.json(), f)
+
     f.write(']')
--- crawler/todb.py
+++ crawler/todb.py
@@ -1,18 +1,26 @@
 import json
 import requests
+# from elasticsearch import Elasticsearch
+# es = Elasticsearch(['http://elastic:uEFp16B1cfVjCSjSJu3j@localhost:9200'])
 
-
-myFile=open("movie.json", 'r')
-myObject=myFile.read()
+myFile = open("movie0.json", 'r')
+myObject = myFile.read()
 u = myObject.decode('utf-8-sig')
 myObject = u.encode('utf-8')
 myFile.encoding
 myFile.close()
-arr=json.loads(myObject,'utf-8')
+arr = json.loads(myObject, 'utf-8')
 
 
 headers = {'Content-Type': 'application/json'}
 
+num = 0
+
 for s in arr:
+    num = num+1
+    if num % 1000 == 0:
+        print(num)
+    #res = es.index(index="moviedb", doc_type='movie', id=s['id'], body=json.dumps(s))
     url = 'http://localhost:9200/database/movies/'+str(s['id'])
-    r = requests.post(url, headers=headers, data=json.dumps(s))
+    r = requests.post(url, headers=headers, data=json.dumps(s), auth=('elastic', 'uEFp16B1cfVjCSjSJu3j'))
+
